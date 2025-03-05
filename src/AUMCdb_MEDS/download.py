4c717fdf641220416de14a6787414e2e1aa6708b
Patrick Rockenschaub <rockenschaub.patrick@gmail.com>
2025-03-05 12:47:46 +0100

    use urls dict like defined in template

--- src/AUMCdb_MEDS/download.py
+++ src/AUMCdb_MEDS/download.py
@@ -11,6 +11,7 @@ from .commands import run_command
 def download_data(
     output_dir: Path,
     dataset_info: DictConfig,
+    do_demo: bool = False,
     runner_fn: callable = run_command,
 ):
     """Downloads the data specified in dataset_info.dataset_urls to the output_dir.
@@ -18,7 +19,7 @@ def download_data(
     Args:
         output_dir: The directory to download the data to.
         dataset_info: The dataset information containing the URLs to download.
-        do_demo: If True, download the demo URLs instead of the main URLs.
+        do_demo: Not currently available for AUMCdb; retained for compatibility but will raise an error if True.
         runner_fn: The function to run the command with (added for dependency injection).
 
     Raises:
@@ -26,30 +27,37 @@ def download_data(
 
     Examples:
         >>> cfg = DictConfig({
-        ...     "dataset_url": "http://example.com/dataset",
-        ...     "api_key": "[MY_API_KEY]"
+        ...     "urls": {
+        ...         "dataset": {
+        ...             "url": "http://example.com/dataset",
+        ...             "api_key": "abcdefg123"
+        ...         }
+        ...     }
         ... })
         >>> def fake_shell_succeed(cmd):
         ...     print(" ".join(cmd))
-        >>> def fake_shell_fail(cmd):
-        ...     raise ValueError(f"Failed to run {' '.join(cmd)}")
         >>> download_data(Path("data"), cfg, runner_fn=fake_shell_succeed)
-        curl -L -o data/AUMCdb.zip -H X-Dataverse-key:[MY_API_KEY] http://example.com/dataset
+        curl -L -o data/AUMCdb.zip -H X-Dataverse-key:abcdefg123 http://example.com/dataset
     """
 
-    url = dataset_info.get("dataset_url", None)
-    if url is None:
-        url = getpass("Enter the download link: ")
+    if do_demo:
+        raise ValueError("Demo download is not currently available for AUMCdb.")
+    else:
+        urls = dataset_info.urls.get("dataset", {})
+        
+        url = urls.get("url", None)
+        if url is None:
+            url = input("Enter the download link: ")
+        
+        key = urls.get("api_key", None)
+        if key is None:
+            key = getpass("Enter your API Token: ")
 
     output_file = output_dir / "AUMCdb.zip"
 
     if output_file.exists():
         logging.info(f"Removing existing file {output_file}")
         output_file.unlink()
-
-    key = dataset_info.get("api_key", None)
-    if key is None:
-        key = getpass("Enter your API Token: ")
 
     command_parts = ["curl", "-L", "-o", str(output_file), "-H", f"X-Dataverse-key:{key}", url]
 
